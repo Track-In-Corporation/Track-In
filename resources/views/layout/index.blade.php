2122cff6341f05db75979bf7ee29d1aafb4ed39d
Specticall <josephyusmita@gmail.com>
2025-11-27 15:58:03 +0700

    fix: import

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
 
-  <link rel="stylesheet" href="{{ asset('bootstrap5.3/css/bootstrap.min.css') }}">
-  <link rel="stylesheet" href="../../css/index.css">
-  @vite('resources/css/app.css')
-</head>
-<body>
-  @include('layout.navbar')
-  @yield('content')
-  @include('layout.footer')
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+
+        <link rel="stylesheet" href="{{ asset("bootstrap5.3/css/bootstrap.min.css") }}">
+        <link rel="stylesheet" href="../../css/index.css">
+        @vite(["resources/css/app.css", "resources/js/app.js"])
+    </head>
+
+    <body>
+        @include("layout.navbar")
+        @yield("content")
+        @include("layout.footer")
+    </body>
+
 </html>
