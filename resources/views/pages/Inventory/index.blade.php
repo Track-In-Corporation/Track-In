2f7fbfc5ab6439c215aa603d8d8aa78da98a8fe7
Specticall <josephyusmita@gmail.com>
2025-12-02 20:40:50 +0700

    feat: enable server https

--- resources/views/pages/Inventory/index.blade.php
+++ resources/views/pages/Inventory/index.blade.php
@@ -1,10 +1,10 @@
-@extends("layout.index")
+@extends('layout.index')
 
-@section("content")
-    @include("pages.inventory.details.index")
+@section('content')
+    @include('pages.inventory.details.index')
     <section class="h-full flex flex-col">
-        @include("pages.inventory.header")
-        @include("pages.inventory.utils")
-        @include("pages.inventory.table")
+        @include('pages.inventory.header')
+        @include('pages.inventory.utils')
+        @include('pages.inventory.table')
     </section>
 @endsection
