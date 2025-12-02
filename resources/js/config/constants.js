0eb199d1118e0dd490e5a90191c50c7e88b90d9f
Specticall <josephyusmita@gmail.com>
2025-12-02 20:19:51 +0700

    feat: app url env

--- resources/js/config/constants.js
+++ resources/js/config/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_APP_URL;
 export const API_URL = `${BASE_URL}/api`;
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    base:
+        process.env.NODE_ENV === "production" ? process.env.APP_URL + "/" : "/",
 });
